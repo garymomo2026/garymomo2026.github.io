e67889baf216f51d628fcc2e1c04501565d61ef9
garymomo2026 <garymomo2026@gmail.com>
2025-01-02 21:50:08 +0800

    init

---
+++ js/general/custom/nav-extra.js
@@ -0,0 +1,117 @@
+﻿document.writeln("<nav class='navbar navbar-default'>");
+document.writeln("	<div class='container-fluid'>");
+document.writeln("		<div class='navbar-header'>");
+document.writeln("			<button type='button' class='navbar-toggle' id='navbar-toggle-custom' data-toggle='collapse' data-target='#momoNavbar'>");
+document.writeln("				<span class='icon-bar'></span>");
+document.writeln("				<span class='icon-bar'></span>");
+document.writeln("				<span class='icon-bar'></span>");
+document.writeln("			</button>");
+document.writeln("			<a class='navbar-brand' href='https://garymomoindex.github.io/?visit=true'><img src='../image/icon/MCZ_PNG.png' title='回首頁'></a>");
+document.writeln("		</div>");
+document.writeln("		<div class='collapse navbar-collapse' id='momoNavbar'>");
+document.writeln("			<ul class='nav navbar-nav'>");
+
+document.writeln("				<li class='dropdown'>");
+document.writeln("					<a class='dropdown-toggle' data-toggle='dropdown'>穿越 -時-<span class='caret'></span></a>");
+document.writeln("					<ul class='dropdown-menu' id='momoNavTime'>");
+document.writeln("						<li><a href='https://garymomo2024.github.io/'>2024</a></li>");
+document.writeln("						<li><a href='https://garymomo2023.github.io/'>2023</a></li>");
+document.writeln("						<li><a href='https://garymomo2022.github.io/'>2022</a></li>");
+document.writeln("						<li><a href='https://garymomo2021.github.io/'>2021</a></li>");
+document.writeln("						<li><a href='https://garymomo2020.github.io/'>2020</a></li>");
+document.writeln("						<li><a href='https://garymomo2019.github.io/'>2019</a></li>");
+document.writeln("						<li><a href='https://garymomo2018.github.io/'>2018</a></li>");
+document.writeln("						<li><a href='https://garymomo2017.github.io/'>2017</a></li>");
+document.writeln("						<li><a href='https://garymomo2016.github.io/redirection/'>2016</a></li>");
+document.writeln("						<li><a href='https://garymomo2015.github.io/'>2015</a></li>");
+document.writeln("						<li><a href='https://garymomo2014.github.io/'>2014</a></li>");
+document.writeln("						<li><a href='https://garymomo2013.github.io/'>2013</a></li>");
+document.writeln("						<li><a href='https://garymomo2012.github.io/'>2012</a></li>");
+document.writeln("						<li><a href='https://garymomo2011.github.io/'>2011</a></li>");
+document.writeln("						<li><a href='https://garymomo2010.github.io/'>2010</a></li>");
+document.writeln("						<li><a href='https://garymomo2009.github.io/'>2009</a></li>");
+document.writeln("						<li><a href='https://garymomo2008.github.io/'>2008</a></li>");
+document.writeln("					</ul>");
+document.writeln("				</li>");
+
+document.writeln("				<li class='dropdown'>");
+document.writeln("					<a class='dropdown-toggle' data-toggle='dropdown'>四葉 -絆-<span class='caret'></span></a>");
+document.writeln("					<ul class='dropdown-menu' id='momoNavMember'>");
+document.writeln("						<li><a href='https://garymomoindex.github.io/member/yellow'>-若大將- 玉井詩織</a></li>");
+document.writeln("						<li><a href='https://garymomoindex.github.io/member/red'>-太陽神- 百田夏菜子</a></li>");
+document.writeln("						<li><a href='https://garymomoindex.github.io/member/pink'>-BOSS- 佐佐木彩夏</a></li>");
+document.writeln("						<li><a href='https://garymomoindex.github.io/member/purple'>-鋼鐵妹- 高城蕾妮</a></li>");
+document.writeln("					</ul>");
+document.writeln("				</li>");
+
+document.writeln("				<li class='dropdown'>");
+document.writeln("					<a class='dropdown-toggle' data-toggle='dropdown'>傾聽 -音-<span class='caret'></span></a>");
+document.writeln("					<ul class='dropdown-menu'>");
+document.writeln("						<li class='dropdown-submenu'><a>依年份<span class='caret'></span></a>");
+document.writeln("							<ul class='dropdown-menu'>");
+document.writeln("								<li><a href='https://garymomo2024.github.io/music'>&nbsp;&nbsp;&nbsp;2024</a></li>");
+document.writeln("								<li><a href='https://garymomo2023.github.io/music'>&nbsp;&nbsp;&nbsp;2023</a></li>");
+document.writeln("								<li><a href='https://garymomo2022.github.io/music'>&nbsp;&nbsp;&nbsp;2022</a></li>");
+document.writeln("								<li><a href='https://garymomo2021.github.io/music'>&nbsp;&nbsp;&nbsp;2021</a></li>");
+document.writeln("								<li><a href='https://garymomo2020.github.io/music'>&nbsp;&nbsp;&nbsp;2020</a></li>");
+document.writeln("								<li><a href='https://garymomo2019.github.io/music'>&nbsp;&nbsp;&nbsp;2019</a></li>");
+document.writeln("								<li><a href='https://garymomo2018.github.io/music'>&nbsp;&nbsp;&nbsp;2018</a></li>");
+document.writeln("								<li><a href='https://garymomo2017.github.io/music'>&nbsp;&nbsp;&nbsp;2017</a></li>");
+document.writeln("								<li><a href='https://garymomo2016.github.io/redirection/music'>&nbsp;&nbsp;&nbsp;2016</a></li>");
+document.writeln("								<li><a href='https://garymomo2015.github.io/music'>&nbsp;&nbsp;&nbsp;2015</a></li>");
+document.writeln("								<li><a href='https://garymomo2014.github.io/music'>&nbsp;&nbsp;&nbsp;2014</a></li>");
+document.writeln("								<li><a href='https://garymomo2013.github.io/music'>&nbsp;&nbsp;&nbsp;2013</a></li>");
+document.writeln("								<li><a href='https://garymomo2012.github.io/music'>&nbsp;&nbsp;&nbsp;2012</a></li>");
+document.writeln("								<li><a href='https://garymomo2011.github.io/music'>&nbsp;&nbsp;&nbsp;2011</a></li>");
+document.writeln("								<li><a href='https://garymomo2010.github.io/music'>&nbsp;&nbsp;&nbsp;2010</a></li>");
+document.writeln("								<li><a href='https://garymomo2009.github.io/music'>&nbsp;&nbsp;&nbsp;2009</a></li>");
+document.writeln("								<li><a href='https://garymomo2008.github.io/music'>&nbsp;&nbsp;&nbsp;2008</a></li>");
+document.writeln("							</ul>");
+document.writeln("						</li>");
+document.writeln("						<li><a>依曲風</a></li>");
+document.writeln("					</ul>");
+document.writeln("				</li>");
+
+document.writeln("				<li class='dropdown'>");
+document.writeln("					<a class='dropdown-toggle' data-toggle='dropdown'>情誼 -緣-<span class='caret'></span></a>");
+document.writeln("					<ul class='dropdown-menu'>");
+document.writeln("						<li class='dropdown-submenu'><a>昔日回憶<span class='caret'></span></a>");
+document.writeln("							<ul class='dropdown-menu'>");
+document.writeln("								<li><a href='https://garymomoindex.github.io/oldmember/green'>-小巨人- 有安杏果</a></li>");
+document.writeln("								<li><a href='https://garymomoindex.github.io/oldmember/blue'>-阿卡玲- 早見朱莉</a></li>");
+document.writeln("								<li><a class='text-white-50' href='https://garymomoindex.github.io/oldmember/m5'>柏幸奈</a></li>");
+document.writeln("								<li><a class='text-white-50' href='https://garymomoindex.github.io/oldmember/m4'>伊倉愛美</a></li>");
+document.writeln("								<li><a class='text-white-50' href='https://garymomoindex.github.io/oldmember/m3'>和川ミユウ（和川未優）</a></li>");
+document.writeln("								<li><a class='text-white-50' href='https://garymomoindex.github.io/oldmember/m2'>藤白すみれ</a></li>");
+document.writeln("								<li><a class='text-white-50' href='https://garymomoindex.github.io/oldmember/m1'>高井つき奈</a></li>");
+document.writeln("								<li><a class='text-white-50' href='https://garymomoindex.github.io/oldmember/m0'>川又智菜美（弓川留奈）</a></li>");
+document.writeln("							</ul>");
+document.writeln("						</li>");
+document.writeln("						<li><a>桃草武士</a></li>");
+document.writeln("					</ul>");
+document.writeln("				</li>");
+
+document.writeln("				<li class='dropdown'>");
+document.writeln("					<a class='dropdown-toggle' data-toggle='dropdown'>台灣 -愛-<span class='caret'></span></a>");
+document.writeln("					<ul class='dropdown-menu' id='momoNavMember'>");
+document.writeln("						<li><a href='https://garymomo2019.github.io/live-taipei'>2019年 台北 -永豐 Legacy Taipei-</a></li>");
+document.writeln("					</ul>");
+document.writeln("				</li>");
+
+document.writeln("				<li><a href='https://garymomoindex.github.io/link'>情報 -繫-</a></li>");
+document.writeln("				<li><a href='https://garymomoindex.github.io/about'>關於</a></li>");
+document.writeln("			</ul>");
+document.writeln("		</div>");
+document.writeln("	</div>");
+document.writeln("</nav>");
+
+$(document).ready(function() {
+	$(".dropdown-submenu>a").on("click tap", function(e) {
+		$(this).next("ul").toggle();
+		e.stopPropagation();
+	});
+
+	$('#momoNavbar').on("hide.bs.dropdown", function () {
+		$(".dropdown-submenu .dropdown-menu").hide();
+	});
+});
